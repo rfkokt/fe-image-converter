b70779293c4faff29970a02520b27966ba94e4f1
Rifki Okta <rifkiokta105@gmail.com>
2025-05-19 15:53:16 +0700

    Fix : change with .env

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -3,12 +3,13 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
   const formData = await req.formData();
-
-  // Kirim formData ke backend Python (http://localhost:3001/convert)
-  const response = await fetch("http://localhost:8000/convert-batch", {
-    method: "POST",
-    body: formData,
-  });
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BE_URL}/convert-batch`,
+    {
+      method: "POST",
+      body: formData,
+    }
+  );
 
   const blob = await response.blob();
 
